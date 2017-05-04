1000f78b3fa5c85fea8633f96db48c242ed3527f
Ivar Nilsen <post@ivarnilsen.com>
2017-05-04 19:38:22 +0200

    Heroku stuff

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, '..', 'node_modules', 'ninno-client', 'public')));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     /* eslint-disable no-console */
-    console.log('App listening on port 3000');
+    console.log('App listening on port', app.get('port'));
     /* eslint-enable no-console */
 });
