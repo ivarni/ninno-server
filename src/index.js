9e5cbcfc18e8afdea7c9df25edee7ba934cd5b33
Ivar Nilsen <post@ivarnilsen.com>
2017-05-05 08:52:01 +0200

    history API fallback

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
-const express = require('express');
 const path = require('path');
+const express = require('express');
+const fallback = require('express-history-api-fallback');
 
 const app = express();
+const root = path.join(__dirname, '..', 'node_modules', 'ninno-client', 'public');
 
-app.use(express.static(path.join(__dirname, '..', 'node_modules', 'ninno-client', 'public')));
+app.use(express.static(root));
+app.use(fallback('index.html', { root }));
 
-app.listen(process.env.PORT || 3000, () => {
+app.set('port', process.env.PORT || 3000);
+app.listen(app.get('port'), () => {
     /* eslint-disable no-console */
     console.log('App listening on port', app.get('port'));
     /* eslint-enable no-console */
