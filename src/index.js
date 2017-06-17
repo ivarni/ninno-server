a2c4cd00f7d31ec63791b7a76ffa5f30651f64d6
Ivar Nilsen <post@ivarnilsen.com>
2017-06-17 12:16:57 +0200

    Enforce SSL in production, closes #1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,19 @@ const express = require('express');
 const fallback = require('express-history-api-fallback');
 
 const app = express();
+
+/* eslint-disable global-require */
+if (process.env.NODE_ENV === 'production') {
+    const enforce = require('express-sslify');
+    app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
+/* eslint-enable global-require */
+
 const root = path.join(__dirname, '..', 'node_modules', 'ninno-client', 'public');
 
 app.use(express.static(root));
 app.use(fallback('index.html', { root }));
+
 
 app.set('port', process.env.PORT || 3000);
 app.listen(app.get('port'), () => {
